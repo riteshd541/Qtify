77f57b806a81d56e4a10a9e199a585ca0a89cd50
riteshd541 <riteshd541@gmail.com>
2023-08-22 12:56:01 +0530

    git commit -m “C3: Button”

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from '../Button/Button'
+
+const NavBar = () => {
+  return (
+    <nav> <Button children="Give Feedback" /></nav>
+  )
+}
+
+export default NavBar
